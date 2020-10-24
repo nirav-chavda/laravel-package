b09b6defe8d04ecb0e60f9a1a8867cb8606fe30f
Nirav Chavda <niravdchavda@gmail.com>
2020-10-24 17:26:11 +0530

    register content moved to boot method

--- src/TimeZoneServiceProvider.php
+++ src/TimeZoneServiceProvider.php
@@ -13,13 +13,6 @@ class TimeZoneServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        /*
-            To distinguish between the default Laravel views and package views, we have to add an extra parameter to our 
-            loadviewsfrom() function and that extra parameter should be the name of your package. In our case, it's timezone. 
-            So now whenever we want to load a view we reference it with this packagename::view syntax convention.
-        */
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'timezone');
     }
 
     /**
@@ -29,6 +22,14 @@ class TimeZoneServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        /*
+            To distinguish between the default Laravel views and package views, we have to add an extra parameter to our 
+            loadviewsfrom() function and that extra parameter should be the name of your package. In our case, it's timezone. 
+            So now whenever we want to load a view we reference it with this packagename::view syntax convention.
+        */
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'timezone');
+        
         /* 
          * Copy our views into Laravel folder resources/views
          * And then, to perform actual copying, user should publish our views, with Artisan command:
